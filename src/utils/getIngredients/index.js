e638de6c4d6699e0e4dc5a68e6bf65dbdaf60fb0
antritue <antritue@gmail.com>
2023-04-03 13:38:47 +0700

    update get list ingredients

--- src/utils/getIngredients/index.js
+++ src/utils/getIngredients/index.js
@@ -1,11 +1,9 @@
 module.exports.getIngredients = (drink) => {
     //get a list of ingredients of a drink
     const ingredients = [];
-    for (let i = 1; i <= 15; i++) {
+    let i = 1
+    while(drink[`strIngredient${i}`]){
         const ingredientName = drink[`strIngredient${i}`];
-        if (!ingredientName) {
-            break;
-        }
         const ingredientMeasure = drink[`strMeasure${i}`];
         //replace white space between name of ingredient with %20
         const ingredientNameParameter = encodeURIComponent(ingredientName.toLowerCase())
@@ -15,6 +13,8 @@ module.exports.getIngredients = (drink) => {
             measure: ingredientMeasure,
             image: `https://www.thecocktaildb.com/images/ingredients/${ingredientNameParameter}.png`,
         })
+        console.log(i);
+        i++
     }
     return ingredients
 };
